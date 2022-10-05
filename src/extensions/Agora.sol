6f9d996394449d0fa3cccaf251d7d26fd18f784c
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-10-06 00:19:43 +0200

    result y/n with bitwise op

--- src/extensions/Agora.sol
+++ src/extensions/Agora.sol
@@ -5,11 +5,7 @@ pragma solidity ^0.8.16;
 import "../guards/CoreGuard.sol";
 
 contract Agora is CoreGuard {
-    event VoteParamsChanged(
-        bytes4 indexed voteId,
-        Consensus indexed consensus,
-        bool indexed added
-    );
+    event VoteParamsChanged(bytes4 indexed voteId, bool indexed added); // add consensus?
 
     event ProposalSubmitted(
         bytes4 indexed slot,
@@ -20,31 +16,39 @@ contract Agora is CoreGuard {
 
     enum ProposalStatus {
         UNKNOWN,
-        EXISTS,
+        ONGOING,
         SUSPENDED,
         ACCEPTED,
         REJECTED
     }
     enum Consensus {
         NO_VOTE,
-        TOKEN,
-        MEMBER
+        TOKEN, // take vote weigth
+        MEMBER // 1 address = 1 vote
+    }
+
+    enum VoteType {
+        YES_NO, // score = (uint128,uint128) = (y,n)
+        PREFERENCE, // score = (uint8,uint8,uint8, ...) = (1,2,3, ...)
+        PERCENTAGE // score = 0 <-> 10000 = (0% <-> 100,00%)
     }
 
     struct VoteParam {
         Consensus consensus;
+        VoteType voteType;
         uint64 votingPeriod;
         uint64 gracePeriod;
         uint64 threshold;
         bool adminValidation;
+        uint256 utilisation;
     }
 
     struct Proposal {
         bytes4 slot;
-        bytes28 proposalId;
+        bytes28 proposalId; // not useful
         uint64 startTime;
         uint64 endTime;
-        uint128 score;
+        uint256 score;
         ProposalStatus status;
         VoteParam params;
         address initiater;
@@ -52,6 +56,7 @@ contract Agora is CoreGuard {
 
     mapping(bytes32 => Proposal) public proposals;
     mapping(bytes4 => VoteParam) public voteParams;
+    mapping(bytes32 => mapping(address => bool)) public votes;
 
     constructor(address core) CoreGuard(core, Slot.AGORA) {}
 
@@ -66,9 +71,7 @@ contract Agora is CoreGuard {
         require(vote.votingPeriod > 0, "Agora: unknown vote params");
 
         if (startTime == 0) startTime = uint64(block.timestamp);
-        require(
-            startTime >= block.timestamp, "Agora: wrong starting time"
-        );
+        require(startTime >= block.timestamp, "Agora: wrong starting time");
 
         proposals[bytes32(bytes.concat(slot, proposalId))] = Proposal(
             slot,
@@ -76,7 +79,7 @@ contract Agora is CoreGuard {
             startTime,
             startTime + vote.votingPeriod,
             0,
-            ProposalStatus.EXISTS,
+            ProposalStatus.ONGOING,
             vote,
             initiater
         );
@@ -87,28 +90,59 @@ contract Agora is CoreGuard {
     function changeVoteParams(
         bytes4 voteId,
         Consensus consensus,
+        VoteType voteType,
         uint64 votingPeriod,
         uint64 gracePeriod,
         uint64 threshold,
         bool adminValidation
     ) external onlyAdapter(Slot.VOTING) {
-        VoteParam memory vote = voteParams[voteId];
-
-        // delete vote params
         if (consensus == Consensus.NO_VOTE) {
-            // check vote params utilisation
-            delete voteParams[voteId];
+            _removeVoteParam(voteId);
         } else {
-            require(
-                vote.consensus != Consensus.NO_VOTE,
-                "Agora: cannot replace voteId"
+            _addVoteParam(
+                voteId,
+                consensus,
+                voteType,
+                votingPeriod,
+                gracePeriod,
+                threshold,
+                adminValidation
             );
         }
     }
 
+    function submitVote(
+        bytes32 proposalId,
+        address voter,
+        uint256 voteWeight,
+        uint256 value
+    ) external onlyAdapter(Slot.VOTING) {
+        _submitVote(proposalId, voter, voteWeight, value);
+    }
+
+    // GETTERS
+    function getProposal(bytes32 proposalId)
+        external
+        view
+        returns (Proposal memory)
+    {
+        return proposals[proposalId];
+    }
+
+    function getVoteParams(bytes4 voteId)
+        external
+        view
+        returns (VoteParam memory)
+    {
+        return voteParams[voteId];
+    }
+
+    // INTERNAL FUNCTION
+
     function _addVoteParam(
         bytes4 voteId,
         Consensus consensus,
+        VoteType voteType,
         uint64 votingPeriod,
         uint64 gracePeriod,
         uint64 threshold,
@@ -119,6 +153,7 @@ contract Agora is CoreGuard {
             vote.consensus == Consensus.NO_VOTE,
             "Agora: cannot replace params"
         );
+
         require(votingPeriod > 0, "Agora: below min period");
         require(
             threshold <= 10000,
@@ -126,15 +161,55 @@ contract Agora is CoreGuard {
         );
 
         vote.consensus = consensus;
+        vote.voteType = voteType;
         vote.votingPeriod = votingPeriod;
         vote.gracePeriod = gracePeriod;
         vote.threshold = threshold;
         vote.adminValidation = adminValidation;
 
-        emit VoteParamsChanged(voteId, consensus, true);
+        emit VoteParamsChanged(voteId, true);
     }
 
-    function _removeVoteParam() internal {
-        // check use !
+    function _removeVoteParam(bytes4 voteId) internal {
+        uint256 utilisation = voteParams[voteId].utilisation;
+        require(utilisation == 0, "Agora: parameters still used");
+
+        delete voteParams[voteId];
+        emit VoteParamsChanged(voteId, false);
+    }
+
+    function _submitVote(
+        bytes32 proposalId,
+        address voter,
+        uint256 voteWeight,
+        uint256 value
+    ) internal {
+        Proposal memory p = proposals[proposalId];
+        require(p.status == ProposalStatus.ONGOING, "Agora: unknown proposal");
+        require(
+            p.startTime <= block.timestamp && p.endTime > block.timestamp,
+            "Agora: outside voting period"
+        );
+
+        require(!votes[proposalId][voter], "Agora: proposal voted");
+        votes[proposalId][voter] = true;
+
+        uint256 score = p.score;
+        if (p.params.voteType == VoteType.YES_NO) {
+            require(value <= 1, "Agora: vote value not match");
+            uint128 tempScore = uint128(score >> 128);
+            // score = value == 0 ?
+        }
+        // is y/n? is preference? is percentage
+        // is by token? is by membership?
+    }
+
+    function _yesNoUtils(uint256 score)
+        internal
+        pure
+        returns (uint128 yes, uint128 no)
+    {
+        yes = uint128(score << 128);
+        no = uint128(score);
     }
 }
---
+++ test/utils.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+import "forge-std/Test.sol";
+import "src/helpers/ScoreUtils.sol";
+
+contract Utils_test is Test {
+    using ScorePackage for uint256;
+
+    function setUp() public {}
+
+    function testBytes() public {
+        // store y/n score
+        uint256 score;
+
+        score = score.incrementYN(6, 0);
+
+        emit log_uint(score.readYes());
+    }
+
+    function _join(uint256 yes, uint256 no) internal pure returns (uint256) {
+        return (yes << 128) | no;
+    }
+
+    function _split(uint256 score)
+        internal
+        pure
+        returns (uint128 yes, uint128 no)
+    {
+        yes = uint128(score >> 128);
+        no = uint128(score);
+    }
+
+    function _incrementScore(
+        uint256 score,
+        uint128 yes,
+        uint128 no
+    ) internal pure returns (uint256) {
+        require((yes == 0 || no == 0) && yes != no, "Score: impossible");
+        (uint128 _yes, uint128 _no) = _split(score);
+        unchecked {
+            _yes += yes;
+            _no += no;
+        }
+        return _join(uint256(_yes), uint256(_no));
+    }
+}
