20f6682dc91b3eed9a07528818c84148c7ce07b5
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-11-21 13:33:22 +0100

    address not spending gas in tests

--- test/Deployment.t.sol
+++ test/Deployment.t.sol
@@ -3,39 +3,122 @@
 pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
+import "forge-std/StdJson.sol";
 
 import "src/core/DaoCore.sol";
+import "src/extensions/Agora.sol";
+import "src/extensions/Bank.sol";
+import "src/adapters/Financing.sol";
+import "src/adapters/Onboarding.sol";
+import "src/adapters/Managing.sol";
+import "src/adapters/Voting.sol";
+
+import "./MockERC20.sol";
 
 contract Deployment_test is Test {
+    using stdJson for string;
+
+    // json file
+    struct Network {
+        address[] admins;
+        address deployer;
+        address[] members;
+        string name;
+        address tokenTBIO;
+    }
+    struct Json {
+        Network[] networks;
+    }
+
     // address
     address[] internal ADMINS;
+    address internal DEPLOYER;
     address[] internal MEMBERS;
+    address internal TBIO;
 
     // contracts
     DaoCore internal dao;
     address internal DAO;
 
+    Agora internal agora;
+    address internal AGORA;
+
+    Bank internal bank;
+    address internal BANK;
+
+    Voting internal voting;
+    address internal VOTING;
+
+    Financing internal financing;
+    address internal FINANCING;
+
+    Managing internal managing;
+    address internal MANAGING;
+
+    Onboarding internal onboarding;
+    address internal ONBOARDING;
+
+    MockERC20 internal tbio;
+
     function setUp() public {
-        // network config
-        // 0. LOCAL
+        // read JSON file
+        string memory file = vm.readFile("test/deployUtils.json");
+        Json memory json = abi.decode(vm.parseJson(file), (Json));
+        Network memory network = json.networks[1]; // ==> Choose network here <==
+
+        // fork network config
+        // 0. LOCAL => no change
         // 1. ANVIL
+        vm.createSelectFork(network.name);
         // 2. SEPOLIA
+        emit log_named_string("Connected on: ", network.name);
+
         // address input
+        ADMINS = network.admins;
+        MEMBERS = network.members;
+        DEPLOYER = network.deployer;
+        TBIO = network.tokenTBIO;
+
+        emit log_uint(DEPLOYER.balance);
+
+        // deploy TBIO if needed
+        if (TBIO == address(0)) {
+            vm.prank(DEPLOYER);
+            tbio = new MockERC20();
+            TBIO = address(tbio);
+        }
     }
 
     function testDeployment() public {
-        string memory path = "output.txt";
+        vm.startPrank(DEPLOYER);
+        dao = new DaoCore(DEPLOYER);
+        DAO = address(dao);
+        agora = new Agora(DAO);
+        AGORA = address(agora);
+        bank = new Bank(DAO, TBIO);
+        BANK = address(bank);
+        voting = new Voting(DAO);
+        VOTING = address(voting);
+        financing = new Financing(DAO);
+        FINANCING = address(financing);
+        managing = new Managing(DAO);
+        MANAGING = address(managing);
+        onboarding = new Onboarding(DAO);
+        ONBOARDING = address(onboarding);
 
-        // string memory line1 = "first line2";
-        // vm.writeLine(path, line1); =append
-        // emit log_string(vm.readFile(path));
+        dao.changeSlotEntry(Slot.AGORA, AGORA);
+        dao.changeSlotEntry(Slot.BANK, BANK);
+        dao.changeSlotEntry(Slot.VOTING, VOTING);
+        dao.changeSlotEntry(Slot.FINANCING, FINANCING);
+        dao.changeSlotEntry(Slot.ONBOARDING, ONBOARDING);
+        dao.changeSlotEntry(Slot.MANAGING, MANAGING);
 
-        emit log_address(address(501));
-        emit log_address(address(502));
-        emit log_address(address(0));
-        emit log_address(address(1));
-        emit log_address(address(2));
-        emit log_address(address(3));
-        emit log_address(address(4));
+        // assertEq(dao.getSlotContractAddr(Slot.AGORA), AGORA, "slot agora");
+        // assertEq(dao.getSlotContractAddr(Slot.BANK), BANK);
+        // assertEq(dao.getSlotContractAddr(Slot.VOTING), VOTING);
+        // assertEq(dao.getSlotContractAddr(Slot.FINANCING), FINANCING);
+        // assertEq(dao.getSlotContractAddr(Slot.MANAGING), MANAGING);
+        // assertEq(dao.getSlotContractAddr(Slot.ONBOARDING), ONBOARDING);
+        emit log_uint(DEPLOYER.balance);
     }
 }
