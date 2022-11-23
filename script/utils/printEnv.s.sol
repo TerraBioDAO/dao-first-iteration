f538c27475caeb540359da644f7f30c45ad23a01
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-11-23 10:02:06 +0100

    update libs

---
+++ script/utils/printEnv.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/utils/Strings.sol";
+
+import "solidity-stringutils/strings.sol";
+
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+
+contract printEnv is Test, Script {
+    function run() public {
+        // read JSON
+        // write .env file
+    }
+}
